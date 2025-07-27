e81768fe1934685349438a07df235284baf1b749
Sonali Desai <sonalidesai1521@gmail.com>
2025-07-27 20:25:14 +0530

    eslint config for semicolon

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,17 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      // 👇 This enforces semicolons
+      semi: ["error", "always"],
+    },
+  },
 ]);
+
